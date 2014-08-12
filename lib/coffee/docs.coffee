54f55b7ba405d9148b46cb7cb6c2d4473af37838
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-08-11 22:15:29 -0400

    Documentation on sidebar

--- lib/coffee/docs.coffee
+++ lib/coffee/docs.coffee
@@ -1,3 +1,9 @@
+for li in $$('.acss-sidebar li')
+  input = document.createElement 'input'
+  input.type = 'text'
+  input.value = li.children[0].innerHTML
+  li.appendChild input
+
 $('.example-debouncer input').addEventListener 'keyup', ->
   save = ->
     $('.example-debouncer .alert').innerHTML = "You saved! { name: '#{$('.example-debouncer input').value}' }"
@@ -6,3 +12,12 @@ $('.example-debouncer input').addEventListener 'keyup', ->
     , 1000
 
   debounce save, 'save'
+
+$('#editSidebar').addEventListener 'click', ->
+  for li in $$('.acss-sidebar li')
+    li.className = 'edit'
+  setTimeout ->
+    for li in $$('.acss-sidebar li')
+      li.className = ''
+  , 5000
+
--- public/js/docs.js
+++ public/js/docs.js
@@ -1,6 +1,15 @@
-var env;
+var env, input, li, _i, _len, _ref;
 
 env = 'dev';
+
+_ref = $$('.acss-sidebar li');
+for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+  li = _ref[_i];
+  input = document.createElement('input');
+  input.type = 'text';
+  input.value = li.children[0].innerHTML;
+  li.appendChild(input);
+}
 
 $('.example-debouncer input').addEventListener('keyup', function() {
   var save;
@@ -11,4 +20,23 @@ $('.example-debouncer input').addEventListener('keyup', function() {
     }, 1000);
   };
   return debounce(save, 'save');
+});
+
+$('#editSidebar').addEventListener('click', function() {
+  var _j, _len1, _ref1;
+  _ref1 = $$('.acss-sidebar li');
+  for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+    li = _ref1[_j];
+    li.className = 'edit';
+  }
+  return setTimeout(function() {
+    var _k, _len2, _ref2, _results;
+    _ref2 = $$('.acss-sidebar li');
+    _results = [];
+    for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
+      li = _ref2[_k];
+      _results.push(li.className = '');
+    }
+    return _results;
+  }, 5000);
 });
